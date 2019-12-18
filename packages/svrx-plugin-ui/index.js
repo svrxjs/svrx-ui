0cc5eb8f1f13263890b4217844e934c41542d7bc
C.Y.XU <itsxcy@gmail.com>
2019-12-18 16:00:38 +0800

    feat: disable plugin configs

--- packages/svrx-plugin-ui/index.js
+++ packages/svrx-plugin-ui/index.js
@@ -15,32 +15,32 @@ const getBuiltins = (config) => {
     }))
     .filter(schemaFilter);
 };
-const getPlugins = (config) => {
-  const pluginNames = config.getExternalPlugins().map(m => m.getInfo('name'));
-  return pluginNames
-    .filter(name => name !== 'ui')
-    .map((name) => {
-      const schema = config.getPlugin(name).getSchema();
-      const required = [];
-      Object.keys(schema).forEach((key) => {
-        if (schema[key].required) {
-          required.push(key);
-          delete schema[key].required;
-        }
-      });
-      const options = config.getPlugin(name).get();
-      return {
-        key: name,
-        schema: {
-          type: 'object',
-          group: 'Plugin',
-          required,
-          properties: schema,
-        },
-        value: options,
-      };
-    });
-};
+// const getPlugins = (config) => {
+//   const pluginNames = config.getExternalPlugins().map(m => m.getInfo('name'));
+//   return pluginNames
+//     .filter(name => name !== 'ui')
+//     .map((name) => {
+//       const schema = config.getPlugin(name).getSchema();
+//       const required = [];
+//       Object.keys(schema).forEach((key) => {
+//         if (schema[key].required) {
+//           required.push(key);
+//           delete schema[key].required;
+//         }
+//       });
+//       const options = config.getPlugin(name).get();
+//       return {
+//         key: name,
+//         schema: {
+//           type: 'object',
+//           group: 'Plugin',
+//           required,
+//           properties: schema,
+//         },
+//         value: options,
+//       };
+//     });
+// };
 
 module.exports = {
   hooks: {
@@ -50,7 +50,8 @@ module.exports = {
         ui = SvrxUI({
           port: 8002,
           builtins: getBuiltins(config),
-          plugins: getPlugins(config),
+          // plugins: getPlugins(config), remove plugin for ui temporarily
+          plugins: [],
           directory: config.get('root'),
           builtinSet: config.builtinsSet.bind(config),
           pluginSet: config.pluginsSet.bind(config),
